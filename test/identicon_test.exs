841f4edb1e9da802cc50dfd45296f3e66ff9c3b8
Dustin Lahr <dustin.lahr.367@my.csun.edu>
2018-10-01 23:29:47 -0700

    tests for colors and hash conversion

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,18 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "expect hash to match name" do
+    foo = Identicon.convert_to_hash("foobar")
+    bar = Identicon.convert_to_hash("foobar")
+    assert foo == bar
+  end
+
+  test "expect colors in RGB" do
+    name = "foobar"
+    foobar =
+    name
+    |> Identicon.convert_to_hash
+    |> Identicon.colors
+    assert foobar == ["38", "58", "F6"]
   end
 end
