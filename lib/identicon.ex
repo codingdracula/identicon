37a8d82c2f1fd7bbb8e50022d5b43faad0866f73
Dustin Lahr <dustin.lahr.367@my.csun.edu>
2018-10-02 23:26:37 -0700

    better named hash to list for struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,13 +5,18 @@ defmodule Identicon do
 
   def transformation(name) do
     name
-    |> convert_to_hash
+    |> convert_to_list
     |> colors
   end
 
-  def convert_to_hash(name) do
-    hash = :crypto.hash(:md5, name) |> Base.encode16()
-    %Identicon.Image{hash: hash}.hash
+  @doc """
+  Takes a string and converts it a binary and then
+  converts it to a list of numbers from 0-255
+  """
+  def convert_to_list(name) do
+    list = :crypto.hash(:md5, name)
+    |> :binary.bin_to_list
+    %Identicon.Image{list: list}
   end
 
   def colors(hash) do
